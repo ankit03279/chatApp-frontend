a70d0688c6881c3590bf6ed393a961a92426ee15
Ankit K Gautam <ankitanuj032@gmail.com>
2025-09-26 01:25:42 +0530

    Initial commit frontend

---
+++ client.js
@@ -0,0 +1,43 @@
+const socket = io("http://localhost:4000");
+
+const msgContainer = document.querySelector(".container");
+const form = document.querySelector(".message_send");
+const msg = document.querySelector(".message");
+var audio = new Audio('chat_audio.mp3');
+
+
+const append = (message, position, extraClass) => {
+  const msgElement = document.createElement("div");
+  msgElement.innerText = message;
+  msgElement.classList.add("msgbox", position);
+  if (extraClass) msgElement.classList.add(extraClass);
+  msgContainer.append(msgElement);
+  if(position == 'left'){
+    audio.play();
+  }
+};
+
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const message = msg.value;
+  append(`You: ${message}`, "right");
+  socket.emit("send", message);
+  msg.value = "";
+});
+
+
+const name = prompt("Enter your name to join chat");
+socket.emit("new-user-joined", name);
+
+socket.on("user-joined", (name) => {
+  append(`${name} joined the chat`, "left", "notify");
+});
+
+socket.on("recieve", (data) => {
+  append(`${data.name}: ${data.message}`, "left");
+});
+
+socket.on("left", (name) => {
+  append(`${name} left the chat`, "left", "notify");
+});
